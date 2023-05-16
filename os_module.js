e496de741f4863d3ae5012cbdec999afe5ae76c1
Sangam Singh <hritriik@gmail.com>
2023-05-16 11:44:50 +0400

    os module added

---
+++ os_module.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+// console.warn(os.freemem() / (1024 * 1024 * 1024));
+// console.warn(os.totalmem() / (1024 * 1024 * 1024));
+// console.warn(os.hostname());
+// console.warn(os.platform());
+console.warn(os.userInfo());
